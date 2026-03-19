a7fe1725ac7df114281a3b84741db6f9f5ba4ea8
d10F-xy <f01d_@naver.com>
2026-03-19 17:09:40 +0900

    과제1 코딩과테스트완료

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,80 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            label1 = new Label();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            EchoMessenger = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = Color.MediumBlue;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(328, 50);
+            label1.TabIndex = 0;
+            label1.Text = "EchoMeaggenger";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // EchoMessenger
+            // 
+            EchoMessenger.Font = new Font("맑은 고딕", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            EchoMessenger.ForeColor = SystemColors.ControlText;
+            EchoMessenger.FormattingEnabled = true;
+            EchoMessenger.Location = new Point(83, 72);
+            EchoMessenger.Name = "EchoMessenger";
+            EchoMessenger.Size = new Size(622, 279);
+            EchoMessenger.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(83, 383);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(484, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(596, 370);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 46);
+            button1.TabIndex = 8;
+            button1.Text = "전송";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(EchoMessenger);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ListBox EchoMessenger;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                EchoMessenger.Items.Add(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
     }
 }
